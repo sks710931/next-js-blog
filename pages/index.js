8b78b60293a79bad59a0276524ae545ba3eb51ad
Shivam Singh <SSingh@dealersocket.com>
2021-03-27 11:33:05 +0530

    migrated slider

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,23 @@
 import Head from "next/head";
 import { Layout } from "../components/layout";
-import { TextBox } from "../components/textbox/textbox";
 import styles from "../styles/Home.module.scss";
-import { TextArea } from '../components/textarea/textarea';
 import { Subscribe } from "../components/subscribe/subscribe";
+import { Slider } from './../components/slider/slider';
+import { Container } from 'react-bootstrap';
 
 export default function Home() {
   return (
     <Layout>
-      <div className={styles.container}>
+      <div>
         <Head>
           <title>Create Next App</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
         <div className={styles.content}>
-
+          <Container className={styles.container}>
+            <Slider />
+          </Container>
         </div>
         <Subscribe />
       </div>
