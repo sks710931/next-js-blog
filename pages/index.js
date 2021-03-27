c670cc6307477d9d11a75df4a56434da92dcec56
Shivam Singh <SSingh@dealersocket.com>
2021-03-27 11:44:48 +0530

    fetch data on homepage

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@ import Head from "next/head";
 import { Layout } from "../components/layout";
 import styles from "../styles/Home.module.scss";
 import { Subscribe } from "../components/subscribe/subscribe";
-import { Slider } from './../components/slider/slider';
-import { Container } from 'react-bootstrap';
+import { Slider } from "./../components/slider/slider";
+import { Container } from "react-bootstrap";
+import axios from "axios";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <Layout>
       <div>
@@ -19,8 +20,29 @@ export default function Home() {
             <Slider />
           </Container>
         </div>
+        {JSON.stringify(posts)}
         <Subscribe />
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  let data;
+  const response = await axios.get(process.env.NEXT_PUBLIC_ENDPOINT_URL + "/posts");
+  if(response.status===200){
+    return {
+      props: {
+        posts: response.data,
+      },
+    };
+  }
+  else{
+    return {
+      props: {
+        posts: [],
+      },
+    };
+  }
+  
 }
